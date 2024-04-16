f2475ef8aa56f11901b0e1ee3c2074873f0aae2c
talhajamal11 <talhajamal11@gmail.com>
2024-04-16 11:04:05 +0100

    added binary model file

---
+++ code/lecture_1/BinModel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Talha Jamal on 16/04/2024.
+//
+
+#include "BinModel.h"
---
+++ code/lecture_1/BinModel.h
@@ -0,0 +1,8 @@
+//
+// Created by Talha Jamal on 16/04/2024.
+//
+
+#ifndef LECTURE_1_BINMODEL_H
+#define LECTURE_1_BINMODEL_H
+
+#endif //LECTURE_1_BINMODEL_H
